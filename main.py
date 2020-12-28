e5c9c026d634f9e1f8842329167a136b0700f2a6
Wojtek <wojciech.smajda@gmail.com>
2020-12-28 15:58:00 +0100

    fix today

--- main.py
+++ main.py
@@ -10,8 +10,10 @@ import gspread
 
 
 def handler(event): 
+
+	today = str(date.today())
+	
 	with open(f'{os.getenv("FILE_MANAGER_PATH")}/results' + today + '.json', 'r') as f: 
 		gc = gspread.service_account(filename=(f'{os.getenv("FILE_MANAGER_PATH")}/c.json'))
 		sh = gc.open('scrapper').sheet1
 		sh.append_row(f)
-		 		
