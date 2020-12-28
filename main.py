f1b3486ec3e112000e5aa7eb9c4131eef6c820ab
Wojtek <wojciech.smajda@gmail.com>
2020-12-28 15:34:00 +0100

    git ignore + paths

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+import pandas as pd
+import tablib
+import openpyxl
+from datetime import date
+import json
+import logging
+import os 
+import gspread
+
+
+def handler(event): 
+	with open(f'{os.getenv("FILE_MANAGER_PATH")}/results' + today + '.json', 'r') as f: 
+		gc = gspread.service_account(filename=(f'{os.getenv("FILE_MANAGER_PATH")}/c.json'))
+		sh = gc.open('scrapper').sheet1
+		sh.append_row(f)
+		 		
